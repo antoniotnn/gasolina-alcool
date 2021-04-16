e70bbb4ea1fdc4bd24d31ea57d09cfc76548d988
Antonio Pires <tonan@globo.com>
2021-04-16 15:31:06 -0300

    App Gasolina ou Alcool 1.0

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -9,4 +9,30 @@ export class HomePage {
 
   constructor() {}
 
+  protected precoAlcool;
+  protected precoGasolina;
+  public resultado: String = "Resultado";
+
+  calcular() {
+  	//validar se os campos foram preenchidos
+  	if(this.precoAlcool && this.precoGasolina) {
+  		var pAlcool = parseFloat(this.precoAlcool);
+  		var pGasolina = parseFloat(this.precoGasolina);
+  		/*
+  		Cálculo: precoAlcool / precoGasolina
+  		se: resultado >=0.7 melhor gasolina
+  		senão: melhor álcool
+  		*/
+  		var res = pAlcool / pGasolina;
+
+  		if(res>=0.7) {
+  			this.resultado = "Melhor utilizar Gasolina";
+  		} else {
+  			this.resultado = "Melhor utilizar Álcool";
+  		}
+
+  	} else {
+  		this.resultado="Preencha corretamente os campos!";
+  	}
+  }
 }
